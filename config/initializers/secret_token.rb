283a24326b08be8665dd41d9e9241d0b20305656
Kevin Yao <kyao@BELC02HL1WMDV7N.sea.corp.expecn.com>
2012-12-16 20:13:38 -0800

    rails proj created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Natlpark::Application.config.secret_token = '521a2a02c0479117d94ac0e17c95e1dd4c3036f6ec386467dc398b51bade8468a5bb849bf6db0c5c8cb8fadc9a156d89350c30016f3bd4ccb7ee7429c7908d13'
