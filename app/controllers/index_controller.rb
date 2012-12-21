d368881cbd1782927498f0d8ea30204886427383
Kevin Yao <kyao@BELC02HL1WMDV7N.sea.corp.expecn.com>
2012-12-20 19:26:41 -0800

    using respond_with to pass data

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,10 @@
 class IndexController < ApplicationController
 
+  respond_to :html, :json
+
   def index
-    @parks = Park.all.as_json(:except => [ :id, :created_at, :updated_at ])
+    @parks = Park.all
+    respond_with(@parks)
   end
 
 end
