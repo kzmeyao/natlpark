370efbb1ba5e30f3be1dea5dc1afc742c01838be
Kevin Yao <kyao@BELC02HL1WMDV7N.sea.corp.expecn.com>
2012-12-20 17:54:36 -0800

    seed file scrapes wikipedia to prefill national parks model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Seed database with national parks, retrieved from Wikipedia
+
+require 'nokogiri'
+require 'open-uri'
+
+url = 'http://en.wikipedia.org/wiki/List_of_national_parks_of_the_United_States'
+
+data = Nokogiri::HTML(open(url))
+
+first_time = true
+
+data.css("table.wikitable").css("tr").each do |row|
+  if !first_time
+    name = row.css("th").text
+    state = "" + row.css("td")[1].css("a")[0]
+    lat =  "" + row.css("td")[1].css(".geo-dec")[0].text.split(" ")[0]
+    lon =  "" + row.css("td")[1].css(".geo-dec")[0].text.split(" ")[1]
+    date_formed = "" + row.css("td")[2].css("span")[1]
+    description = "" + row.css("td")[4]
+    description.gsub!(/\[.?.\]/, "")
+
+    park = Park.new(:date_formed => date_formed,
+                    :description => description,
+                    :lat => lat, :lon => lon,
+                    :name => name,
+                    :state => state)
+    park.save
+  else
+    first_time = false
+  end
+end
