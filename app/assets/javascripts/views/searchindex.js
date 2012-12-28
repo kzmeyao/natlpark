1bf7e691e6d2d3d82f3259cdfa1ae7244aaa3401
Kevin Yao <kyao@BELC02HL1WMDV7N.sea.corp.expecn.com>
2012-12-27 20:22:21 -0800

    masonry table of contents implemented

--- app/assets/javascripts/views/searchindex.js
+++ app/assets/javascripts/views/searchindex.js
@@ -2,10 +2,17 @@ var SearchIndex = Backbone.View.extend({
 
   displayed: false,
 
-  initialize: function(){
+  initialize: function(parks){
+    _.bindAll(this, 'render', 'appendParksList');
     this.$el.html(
-//      "<div class='box'></div>"
+      "<div id='table-of-contents'>" + "</div>"
     )
+    this.appendParksList();
+    window.onresize = function(){
+      var winW = window.innerWidth;
+      var tableW = Math.floor(winW/240) * 240;
+      $('#table-of-contents').css("width", tableW + "px");
+    };
   },
 
   render: function(){
@@ -13,9 +20,27 @@ var SearchIndex = Backbone.View.extend({
       this.$el.css("display", "block");
       this.$el.attr("class", "overlay slide-in");
       this.displayed = true;
+      $('#table-of-contents').masonry({
+        itemSelector : '.state-list'
+      });
     } else {
       this.$el.attr("class", "overlay slide-out");
       this.displayed = false;
     }
+  },
+
+  appendParksList: function(){
+    var parks = NatlPark.Collections.Parks;
+    var states = _.uniq(NatlPark.Collections.Parks.pluck('state').sort());
+    var toAppend = "";
+    for( var i in states ){
+      toAppend += "<div class='state-list'>" + states[i] + "<ul class='state'>";
+      var stateParks = NatlPark.Collections.Parks.where({'state':states[i]});
+      for( var j in stateParks ){
+        toAppend += "<li>" + stateParks[j].name + "</li>";
+      }
+      toAppend += "</ul></div>";
+    }
+    this.$('#table-of-contents').html(toAppend);
   }
 })
